ea7bd5e36f34a7c8636d14da8458e750fecec03c
itsSamarIbrahim <>
2024-03-22 17:53:39 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 #Installing flask from pip3 using Puppet
 
+# ensure that python present
+package { 'python3.8':
+  ensure => present,
+}
+
+# ensure that pip present
+package { 'python3-pip':
+  ensure => present,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => pip3,
