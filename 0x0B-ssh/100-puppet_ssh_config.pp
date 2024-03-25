e47e3838d73514f8e653683c2f89de0c9b2f2a3c
itsSamarIbrahim <>
2024-03-24 21:55:16 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,8 +5,18 @@
 #Your SSH client configuration must be configured to use the private key ~/.ssh/school
 #Your SSH client configuration must be configured to refuse to authenticate using a password
 
-file { 'Change-file':
-  ensure  => 'file',
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  content => $cont,
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
