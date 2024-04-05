7a37d4a8cef5d3a03af65be1efd9e1fe88fc30a5
itsSamarIbrahim <>
2024-04-04 23:51:41 -0700

    deleted folders

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-#Creating a file in /tmp using Puppet
-
-file { '/tmp/school':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-  }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-#Creating a manifest that kills a process named killmenow using Puppet
-
-exec { 'pkill killmenow':
-  command  => 'pkill killmenow',
-  provider => shell,
-}
