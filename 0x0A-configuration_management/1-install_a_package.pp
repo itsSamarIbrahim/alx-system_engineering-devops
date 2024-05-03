b691d322c3261d611fb7bd2b21baba394506df3a
Samar <bantmasr21@gmail.com>
2024-05-03 11:23:59 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,23 +1,19 @@
 #Using Puppet, install flask from pip3
 
-#Installing flask from pip3 using Puppet
-package {'flask':
-  ensure   => installed,
-  version  => '2.1.0',
-  provider => pip3,
-  require  => Package['python3.8'],
-  require  => Package['python3-pip'],
-}
-
 #Ensuring that python present
-package {'python3.8':
-  ensure  => installed,
-  version => '3.8.10',
+package { 'python3.8':
+  ensure => present,
 }
 
 #Ensuring that pip present
 package { 'python3-pip':
   ensure => present,
+}
+
+#Installing flask from pip3 using Puppet
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
 
 #Installing Werkzeug
