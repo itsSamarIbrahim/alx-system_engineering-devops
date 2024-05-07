fd5060859cb0b4173b4427f6b4468493aa2d85d3
Samar <bantmasr21@gmail.com>
2024-05-07 11:45:37 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,40 +10,40 @@ package { 'nginx':
   install_options => ['-y'],
 }
 
-file_line { 'install':
-  ensure => 'presesnt',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.github.com/itssamaribrahim permanent;',
-}
-
-file { 'Home page':
-  ensure  => 'file',
-  path    => '/var/www/html/index.html',
-  mode    => '0744',
-  owner   => 'www-data',
-  content => 'Hello World!',
-  require => Exec['nginx'],
-}
-
-file { '404 page':
-  ensure  => 'file',
-  path    => '/var/www/error/404.html',
-  mode    => '0744',
-  owner   => 'www-data',
-  content => "Ceci n'est pas une page\n",
-  require => Exec['nginx'],
-}
-
-exec { 'Restart nginx':
-  command => 'service nginx restart',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
-
+#file_line { 'install':
+#  ensure => 'presesnt',
+#  path   => '/etc/nginx/sites-enabled/default',
+#  after  => 'listen 80 default_server;',
+#  line   => 'rewrite ^/redirect_me https://www.github.com/itssamaribrahim permanent;',
+#}
+#
+#file { 'Home page':
+#  ensure  => 'file',
+#  path    => '/var/www/html/index.html',
+#  mode    => '0744',
+#  owner   => 'www-data',
+#  content => 'Hello World!',
+#  require => Exec['nginx'],
+#}
+#
+#file { '404 page':
+#  ensure  => 'file',
+#  path    => '/var/www/error/404.html',
+#  mode    => '0744',
+#  owner   => 'www-data',
+#  content => "Ceci n'est pas une page\n",
+#  require => Exec['nginx'],
+#}
+#
+#exec { 'Restart nginx':
+#  command => 'service nginx restart',
+#}
+#
+#service { 'nginx':
+#  ensure  => running,
+#  require => Package['nginx'],
+#}
+#
 exec { 'run':
   command  => 'sudo service nginx restart',
 }
