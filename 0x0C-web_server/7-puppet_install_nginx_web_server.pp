da9db7061218ee65cde025d3d2a2523bfff0bd64
Samar <bantmasr21@gmail.com>
2024-05-07 12:42:05 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,49 +1,21 @@
 # Setup New Ubuntu server with nginx
 
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
-}
-
 package { 'nginx':
-  ensure          => installed,
-  require         => Exec['update system'],
-#  install_options => ['-y'],
+  ensure => installed,
 }
 
 file_line { 'install':
-  ensure => 'presesnt',
+  ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.github.com/itssamaribrahim permanent;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
 }
 
-file { 'Home page':
-  ensure  => 'file',
-  path    => '/var/www/html/index.html',
-  mode    => '0744',
-  owner   => 'www-data',
+file { '/var/www/html/index.html':
   content => 'Hello World!',
-  require => Exec['nginx'],
 }
 
-#file { '404 page':
-#  ensure  => 'file',
-#  path    => '/var/www/error/404.html',
-#  mode    => '0744',
-#  owner   => 'www-data',
-#  content => "Ceci n'est pas une page\n",
-#  require => Exec['nginx'],
-#}
-#
-#exec { 'Restart nginx':
-#  command => 'service nginx restart',
-#}
-#
-#service { 'nginx':
-#  ensure  => running,
-#  require => Package['nginx'],
-#}
-#
-exec { 'run':
-  command  => 'sudo service nginx restart',
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
