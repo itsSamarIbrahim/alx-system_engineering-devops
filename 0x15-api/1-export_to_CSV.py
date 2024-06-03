0734ef7db06ea638d869b77b7c7ab4b9467c3c24
Samar <bantmasr21@gmail.com>
2024-06-03 15:45:26 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,50 +1,36 @@
 #!/usr/bin/python3
-"""
-Using what you did in the task #0, extend your Python script to export data
-in the CSV format
-"""
+""" 1. Input employee ID, output info to CSV file. """
+
 import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    employee_info_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    todo_list_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+    id = sys.argv[1]
+    info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        id))
+    todo = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
 
-    employee_info_json = employee_info_response.json()
-    todo_list_json = todo_list_response.json()
+    infod = info.json()
+    todod = todo.json()
 
-    employee_name = employee_info_json.get('name')
-    employee_username = employee_info_json.get('username')
-    total_tasks = len(todo_list_json)
+    name = infod.get('name')
+    user_name = infod.get('username')
+    tasks = len(todod)
 
-    completed_tasks = 0
-    for task in todo_list_json:
-        if task.get('completed'):
-            completed_tasks += 1
+    count = 0
+    for comp in todod:
+        finished = comp.get('completed')
+        if finished:
+            count += 1
 
-    with open(f'{employee_id}.csv', 'w', newline='') as employee_tasks_file:
-        writer = csv.writer(
-            employee_tasks_file,
-            delimiter=',',
-            quotechar='"',
-            quoting=csv.QUOTE_ALL)
-
-        writer.writerow(['UserID', 'Username', 'TaskCompleted', 'TaskTitle'])
-
-        for task in todo_list_json:
-            task_user_id = task.get('userId')
-            task_completed = task.get('completed')
-            task_title = task.get('title')
-
-            row_data = [
-                task_user_id,
-                employee_username,
-                task_completed,
-                task_title]
-
-            writer.writerow(row_data)
+    with open('{}.csv'.format(id), 'w') as emp_tasks:
+        emp_writer = csv.writer(emp_tasks, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for task in todod:
+            uid = task.get('userId')
+            comp = task.get('completed')
+            title = task.get('title')
+            write_list = [uid, user_name, comp, title]
+            emp_writer.writerow(write_list)
