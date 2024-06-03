c493e7c48e6277f8cb9bdb25786ce739b18e7113
Samar <bantmasr21@gmail.com>
2024-06-03 14:55:35 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,14 +11,14 @@ import sys
 if __name__ == "__main__":
     employee_id = sys.argv[1]
     employee_info_response = requests.get(
-            f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     todo_list_response = requests.get(
-            f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
 
     employee_info_json = employee_info_response.json()
     todo_list_json = todo_list_response.json()
 
-    employee_name = employee_info_json.get('name')
+    employee_name = employee_info_json.get('name', '')
     total_todo_count = len(todo_list_json)
 
     completed_task_count = 0
@@ -26,9 +26,9 @@ if __name__ == "__main__":
         if task.get('completed'):
             completed_task_count += 1
 
-    print(f'Employee {employee_name}\
-is done with tasks({completed_task_count}/{total_todo_count}):')
+    print(
+        f'Employee {employee_name} is done with tasks({completed_task_count}/{total_todo_count}):')
     for task in todo_list_json:
         if task.get('completed'):
-            task_title = task.get('title')
+            task_title = task.get('title', '')
             print(f'\t {task_title}')
