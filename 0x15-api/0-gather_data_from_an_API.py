fa797d8f284226f66f502c6fde8a5f04ea342f20
Samar <bantmasr21@gmail.com>
2024-06-03 15:00:40 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,28 +8,22 @@ import requests
 import sys
 
 
+def get_data_from_api(uid):
+    """
+    Gets and prints data from JSON PLACEHOLDER API
+    Args:
+        uid: employee id
+    Return:
+        None
+    """
+    base = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base + "users/" + uid).json()
+    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
+    completed = [_.get("title") for _ in userTodos if _.get("completed")]
+    output = "Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(userTodos))
+    print("\n\t ".join([output] + completed))
+
+
 if __name__ == "__main__":
-    id = sys.argv[1]
-    info = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}'.format(id))
-    todo = requests.get(
-            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
-
-    infod = info.json()
-    todod = todo.json()
-
-    name = infod.get('name')
-    tasks = len(todod)
-
-    count = 0
-    for comp in todod:
-        finished = comp.get('completed')
-        if finished:
-            count += 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(name, count, tasks))
-    for task in todod:
-        completed = task.get('completed')
-        if completed:
-            title = task.get('title')
-            print("\t {}".format(title))
+    get_data_from_api(sys.argv[1])
