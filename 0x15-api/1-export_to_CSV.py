f3694cd98aa41c2bf3eb38d592aa446972886865
Samar <bantmasr21@gmail.com>
2024-06-03 17:40:00 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" 1. Input employee ID, output info to CSV file. """
+"""
+Using what you did in the task #0, extend your Python script to export data
+in the CSV format
+"""
+
 
 import csv
 import requests
