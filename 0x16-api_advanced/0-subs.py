bf674b38a564d737099c4498d2db03a18e08baaf
Samar <bantmasr21@gmail.com>
2024-06-20 16:41:07 -0700

    added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,12 +19,15 @@ def number_of_subscribers(subreddit):
            Returns 0 if the subreddit does not exist,
            the request fails, or the status code is not 200.
     """
-    headers = {"User-Agent": "Custom",}
+    headers = {"User-Agent": "Custom", }
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    else:
+    try:
+        if response.status_code == 200:
+            return response.json().get("data").get("subscribers")
+        else:
+            return 0
+    except Exception as e:
         return 0
