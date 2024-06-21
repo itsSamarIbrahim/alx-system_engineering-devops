7ba926965bf82b9236bad4461d898bd0dfccb2a5
Samar <bantmasr21@gmail.com>
2024-06-20 17:51:48 -0700

    added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""
-a function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit. If an invalid
-subreddit is given, the function should return 0
-"""
-import requests
+""" How many subs? """
 
 
 def number_of_subscribers(subreddit):
-    """
-    Fetches the number of subscribers for a given subreddit on Reddit.
+    """ Returns subscriber count of subreddit or 0 """
+    from requests import get
 
-    Parameters:
-    - subreddit (str): The name of the subreddit to fetch subscriber count for.
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    Returns:
-    - int: The number of subscribers for the specified subreddit.
-           Returns 0 if the subreddit does not exist,
-           the request fails, or the status code is not 200.
-    """
-    headers = {"User-Agent": "Custom", }
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'user-agent': 'my-app/0.0.1'}
 
-    response = requests.get(url, headers=headers)
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
 
     try:
-        if response.status_code == 200:
-            return response.json().get("data").get("subscribers")
-        else:
-            return 0
-    except Exception as e:
+        js = r.json()
+
+    except ValueError:
         return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
+    return 0
