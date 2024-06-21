2a7d39f1119695d105d80b58f2f87011dcdc15fe
Samar <bantmasr21@gmail.com>
2024-06-20 17:43:24 -0700

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -57,9 +57,10 @@ def count_words(subreddit, word_list, after=None, word_count=None):
             if after:
                 return count_words(subreddit, word_list, after, word_count)
             else:
-                sorted_word_count =
-                (sorted(word_count.items(),
-                        key=lambda item: (-item[1], item[0])))
+                sorted_word_count = (
+                    sorted(word_count.items(),
+                           key=lambda item: (-item[1], item[0]))
+                )
                 for word, count in sorted_word_count:
                     print(f'{word}: {count}')
                 return
